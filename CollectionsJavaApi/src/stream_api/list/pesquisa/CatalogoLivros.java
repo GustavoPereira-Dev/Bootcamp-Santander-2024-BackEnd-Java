47d240942c4a9b2506db662bdd8488b9e4b1df55
GustavoPereira-Dev <gustavopereira8919@gmail.com>
2024-08-09 23:04:07 -0300

    Atualizando Collections Java API

---
+++ CollectionsJavaApi/src/stream_api/list/pesquisa/CatalogoLivros.java
@@ -0,0 +1,49 @@
+package stream_api.list.pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CatalogoLivros {
+    // atributo
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        if (!livroList.isEmpty()) {
+            return livroList.stream()
+                    .filter(l -> l.getAutor().equalsIgnoreCase(autor))
+                    .collect(Collectors.toList());
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        if (!livroList.isEmpty()) {
+            return livroList.stream()
+                    .filter(l -> l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal)
+                    .collect(Collectors.toList());
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        if (!livroList.isEmpty()) {
+            return livroList.stream()
+                    .filter(l -> l.getTitulo().equalsIgnoreCase(titulo))
+                    .findFirst()
+                    .orElse(null);
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+}
---
+++ CollectionsJavaApi/src/stream_api/set/ordenacao/CadastroProdutos.java
@@ -0,0 +1,39 @@
+package stream_api.set.ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class CadastroProdutos {
+
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(cod, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        if (!produtoSet.isEmpty()) {
+            return produtoSet.stream()
+                    .sorted()
+                    .collect(Collectors.toCollection(TreeSet::new));
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        if (!produtoSet.isEmpty()) {
+            return produtoSet.stream()
+                    .sorted(new ComparatorPorPreco())
+                    .collect(Collectors.toCollection(TreeSet::new));
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+}
